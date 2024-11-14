376ab0579fc3a67a05fe60cd78e136891a74d506
CupChailatte <Chaisoda864@gmail.com>
2024-11-14 11:39:49 +0100

    new starts

---
+++ code.js
@@ -0,0 +1,15 @@
+const myLibrary = [];
+
+//ES6 classes 
+class Book{
+    constructor(title, author, pages, readStatus){
+        this.title =title; 
+        this.author = author; 
+        this.pages = pages; 
+        this.readStatus = readStatus; 
+    }
+
+}
+function addBookToLibrary() {
+  // do stuff here
+}
